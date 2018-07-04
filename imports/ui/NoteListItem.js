6e127097fb6bf49d14e4b8053b4205c805dcb725
Tom <tomek7sa@gmail.com>
2018-07-04 07:42:03 +0200

    Integrate selectedNoteId into list items and url

--- imports/ui/NoteListItem.js
+++ imports/ui/NoteListItem.js
@@ -1,18 +1,26 @@
-import React from 'react';
-import moment from 'moment';
-import { NoteList } from './NoteList';
+import React from "react";
+import moment from "moment";
+import { Session } from "meteor/session";
+import { createContainer } from "meteor/react-meteor-data";
 
-const NoteListItem = (props) => {
-    return (
-        <div>
-            <h5>{props.note.title || 'Untitled note'}</h5>
-            <p>{ moment(props.note.updatedAt).format('M/DD/YY') }</p>
-        </div>
-    );
+export const NoteListItem = props => {
+  return (
+    <div
+      onClick={() => {
+        props.Session.set('selectedNoteId', props.note._id);
+      }}
+    >
+      <h5>{props.note.title || "Untitled note"}</h5>
+      <p>{moment(props.note.updatedAt).format("M/DD/YY")}</p>
+    </div>
+  );
 };
 
 NoteListItem.propTypes = {
-    note: React.PropTypes.object.isRequired
-}
+  note: React.PropTypes.object.isRequired,
+  Session: React.PropTypes.object.isRequired
+};
 
-export default NoteListItem;
+export default createContainer(() => {
+  return { Session };
+}, NoteListItem);

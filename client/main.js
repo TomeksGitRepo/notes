237d2aa5bc1bd381a33bb2201f93e77771b6f570
Tom <tomek7sa@gmail.com>
2018-07-06 07:39:08 +0200

    Refactor router to allow for dynamic urls

--- client/main.js
+++ client/main.js
@@ -12,7 +12,9 @@ import "./main.html";
 
 Tracker.autorun(() => {
   const isAuthenticated = !!Meteor.userId();
-  onAuthChange(isAuthenticated);
+  const currentPagePrivacy = Session.get('currentPagePrivacy');
+
+  onAuthChange(isAuthenticated, currentPagePrivacy);
 });
 
 Tracker.autorun(() => {
